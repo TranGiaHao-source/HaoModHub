28edacb9c419c2f337488483d44c3672725152f0
HaoDz <trangiahao2409@gmail.com>
2026-03-01 21:37:15 +0700

    Update HAOMODHUB_VIP_.lua

--- HAOMODHUB_VIP_.lua
+++ HAOMODHUB_VIP_.lua
@@ -3682,7 +3682,9 @@ local ui_loaded = false
 
 -- Thử load UI từ nhiều nguồn khác nhau
 local ui_urls = {
-    "https://raw.githubusercontent.com/TranGiaHao-source/Test/refs/heads/main/ui_BananaHub_final.lua.txt"
+    "https://pastefy.app/JJynGTps/raw",
+    "https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib",
+    "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Unnamed",
 }
 
 for i, url in ipairs(ui_urls) do
