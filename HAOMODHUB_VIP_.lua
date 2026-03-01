541e668194f792e8e25e8aeaabb7db057d4e3a4f
HaoDz <trangiahao2409@gmail.com>
2026-03-01 21:30:19 +0700

    Update HAOMODHUB_VIP_.lua

--- HAOMODHUB_VIP_.lua
+++ HAOMODHUB_VIP_.lua
@@ -3682,9 +3682,7 @@ local ui_loaded = false
 
 -- Thử load UI từ nhiều nguồn khác nhau
 local ui_urls = {
-    "https://pastefy.app/JJynGTps/raw",
-    "https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib",
-    "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Unnamed",
+    "
 }
 
 for i, url in ipairs(ui_urls) do
